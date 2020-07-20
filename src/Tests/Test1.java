7fc961b8ac4f2aa3051c13796a29f789749cda4c
taydin-ra <ttorun16@gmail.com>
2020-07-20 18:38:34 -0400

    Test1 codes are added

--- src/Tests/Test1.java
+++ src/Tests/Test1.java
@@ -1,7 +1,85 @@
 package Tests;
 
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class Test1 {
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    @BeforeClass
+    void setup() {
+        System.setProperty("webdriver.chrome.driver", "/Users/yavuzaydin/Desktop/selenium/chromedriver 2");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 6);
+        driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+    }
+
+    @BeforeMethod
+    void loginThePage() {
+        driver.get(" https://app.hubspot.com/login");
+        driver.manage().window().maximize();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input#username")));
+        //WebElement userName = driver.findElement(By.xpath("//input[@id='username']"));
+        WebElement username = driver.findElement(By.cssSelector("input#username"));
+        username.sendKeys("haliterdogdu@hotmail.com");
+        WebElement password = driver.findElement(By.cssSelector("input[id='password']"));
+        password.sendKeys("GAAtl12@");
+        //Click on login button
+        WebElement loginButton = driver.findElement(By.xpath("//button[@data-test-id='password-login-button']"));
+        loginButton.click();
+
+    }
+
+    @Test
+    void createContact() {
+        // click on the contacts on the menu tab
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(" (//a[@id='nav-primary-contacts-branch'])[1]")));
+        WebElement contact = driver.findElement(By.xpath(" (//a[@id='nav-primary-contacts-branch'])[1]"));
+        contact.click();
+        // click on contacts that inside the contacts
+        WebElement contacts = driver.findElement(By.xpath("(//div[contains(text(),'Contacts')])[1]"));
+        contacts.click();
+
+        // click on the 'Create Contact' tab
+
+        WebElement createContactButton = driver.findElement(By.xpath("//button[@data-onboarding='new-object-button']"));
+        createContactButton.click();
+
+        // Sending email
+
+        WebElement email = driver.findElement(By.cssSelector("input[data-selenium-test='property-input-email']"));
+        String mail = "atlanta@gmail.com";
+        email.sendKeys(mail);
+
+        // Sending firstName
+
+        String name = "Atlanta";
+        WebElement firstNAme = driver.findElement(By.cssSelector("input[data-selenium-test='property-input-firstname']"));
+        firstNAme.sendKeys(name);
+
+        // Sending the lastname
+
+        WebElement lastNAme = driver.findElement(By.cssSelector("input[data-selenium-test='property-input-lastname']"));
+        lastNAme.sendKeys("is the best");
+
+        // click on create contact
+        WebElement creation = driver.findElement(By.xpath("(//span[text()='Create contact'])[2]"));
+        creation.click();
+
+
+
 
     /*
 
@@ -23,5 +101,5 @@ public class Test1 {
 
      */
 
-
+    }
 }
